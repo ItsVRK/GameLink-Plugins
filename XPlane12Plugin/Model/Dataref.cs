5824bfe3b02a532190e598b26031bf6252451a2c
Dániel Zettisch <zettisch.daniel@yawvr.com>
2025-08-13 14:46:17 +0200

    Implemented UDP Dataref protocol

---
+++ XPlane12Plugin/Model/Dataref.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace XPlane12Plugin.Model
+{
+    public class DatarefValue
+    {
+        public string InputName { get; set; }
+        public string Dataref { get; set; }
+        public int Id { get; set; }
+
+        [JsonIgnore]
+        public float Value { get; set; }
+
+    }
+}
